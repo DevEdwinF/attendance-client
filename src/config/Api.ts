206e35b8a12815a2b8c72c317c15fa7f5f864e13
Edwinfpirajan <efpa1998@hotmail.com>
2023-10-13 16:06:15 -0500

    update api

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,7 +1,7 @@
 export const Api = {
     //url: 'https://api.smart.edu.co'
-     url: 'http://localhost:8080'
-    // url: 'https://test-back-production-5069.up.railway.app'
+    // url: 'http://localhost:8080'
+     url: 'https://test-back-production-5069.up.railway.app'
     //examle
     //examle
     //examle
